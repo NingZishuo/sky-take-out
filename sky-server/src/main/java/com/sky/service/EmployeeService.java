976727ebdabcb23e5a7f4a25556bf8a20f375d00
Ning Zishuo <zishuoningn@gmail.com>
2025-06-11 22:52:09 +0900

    day02已结束 菜品管理自己写

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -23,9 +23,30 @@ public interface EmployeeService {
     void save(EmployeeDTO employeeDTO);
 
     /**
-     *
+     * 员工分页查询
      * @param employeePageQueryDTO
      * @return
      */
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 员工启用禁用
+     * @param status
+     * @param id
+     */
+    void updateStatus(Integer status, Long id);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     */
+    Employee getById(Long id);
+
+    /**
+     * 员工启用编辑
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
+
+
 }
